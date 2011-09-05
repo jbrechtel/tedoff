15cb40c09f06bacd3767f6433fe05640758db04b
James Brechtel <jbrechtel@gmail.com>
2011-09-05 19:05:15 -0400

    Use yahoo yql to convert rss to json; Display returned data

--- src/lib/tedfeed.js
+++ src/lib/tedfeed.js
@@ -9,16 +9,43 @@ enyo.kind({
               {name: "feedUrl", kind: "Input", flex: 1},
               {kind: "Button", caption: "Get Feed", onclick: "btnClick"}
           ]}
+    ]},
+    {kind: "Scroller", flex: 1, components: [
+        {name: "list", kind: "VirtualRepeater", onSetupRow: "getListItem",
+            components: [
+                {kind: "Item", layoutKind: "VFlexLayout", components: [
+                    {name: "title", kind: "Divider"},
+                    {name: "description"}
+                ]}
+            ]
+        }
     ]}
   ],
   btnClick: function() {
-    this.$.getFeed.setUrl("http://www.ted.com/talks/rss");
+    var url = "http://query.yahooapis.com/v1/public/yql?q=select"
+      + "%20title%2C%20description%20from%20rss%20where%20url%3D%22"
+      + "http://www.ted.com/talks/rss%22&format=json&callback=";
+    this.$.getFeed.setUrl(url);
     this.$.getFeed.call();
   },
-  gotFeedSuccess: function() {
-    this.$.button.setCaption("success");
+  gotFeedSuccess: function(inSender, inResponse) {
+    this.results = inResponse.query.results.item;
+    this.$.list.render();
   },
-  gotFeedFailure: function() {
-    this.$.button.setCaption("failed");
+  gotFeedFailure: function(inSender, inResponse) {
+    enyo.log("Failed to get TED video list");
+  },
+  create: function() {
+    this.inherited(arguments);
+    this.results = [];
+  },
+  getListItem: function(inSender, inIndex) {
+    var item = this.results[inIndex];
+    if(!item) {
+      return false;
+    }
+    this.$.title.setCaption(item.title);
+    this.$.description.setContent(item.description);
+    return true;
   }
 });
