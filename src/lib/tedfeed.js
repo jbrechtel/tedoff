3c943653a436ae4cddeda5103d0ba1b7cfbdb482
James Brechtel <jbrechtel@gmail.com>
2011-09-06 21:45:37 -0400

    Download files on click

--- src/lib/tedfeed.js
+++ src/lib/tedfeed.js
@@ -2,6 +2,16 @@ enyo.kind({
   name: "WTFWare.TEDFeed",
   kind: enyo.VFlexBox,
   components: [
+    {
+      name : "fileDownload",
+      kind : "PalmService",
+      service : "palm://com.palm.downloadmanager/",
+      method : "download",
+      onSuccess : "downloadFinished",
+      onFailure : "downloadFail",
+      onResponse : "gotResponse",
+      subscribe : true
+    },
     {name: "getFeed", kind: "WebService", onSuccess: "gotFeedSuccess", onFailure: "gotFeedFailure"},
     {kind: "PageHeader", content: "TED Talks -- Offline Viewer"},
     {kind: "RowGroup", caption: "Feed URL", components: [
@@ -13,7 +23,8 @@ enyo.kind({
     {kind: "Scroller", flex: 1, components: [
         {name: "list", kind: "VirtualRepeater", onSetupRow: "getListItem",
             components: [
-                {kind: "Item", layoutKind: "VFlexLayout", components: [
+                {kind: "Item", layoutKind: "VFlexLayout", onclick: "videoClick",
+                  components: [
                     {name: "title", kind: "Divider"},
                     {name: "description"}
                 ]}
@@ -23,7 +34,7 @@ enyo.kind({
   ],
   btnClick: function() {
     var url = "http://query.yahooapis.com/v1/public/yql?q=select"
-      + "%20title%2C%20description%20from%20rss%20where%20url%3D%22"
+      + "%20enclosure%2C%20title%2C%20description%20from%20rss%20where%20url%3D%22"
       + "http://www.ted.com/talks/rss%22&format=json&callback=";
     this.$.getFeed.setUrl(url);
     this.$.getFeed.call();
@@ -47,5 +58,24 @@ enyo.kind({
     this.$.title.setCaption(item.title);
     this.$.description.setContent(item.description);
     return true;
+  },
+  videoClick: function(inSender, inEvent) {
+    var feed = this.results[inEvent.rowIndex];
+    this.$.fileDownload.call({
+      target: feed.enclosure.url,
+      mime: feed.enclosure.type,
+      keepFilenameOnRedirect: false,
+      canHandlePause: true,
+      subscribe: true
+    });
+  },
+  downloadFinished: function() {
+    enyo.log("downloadFinished...");
+  },
+  downloadFailed: function() {
+    enyo.log("failed...");
+  },
+  onResponse: function() {
+    enyo.log("onResponse...");
   }
 });
